b2250eb3bbd32cad946bd3449a8c29c3c29a4b5d
JavaSelenium-Git <130776988+JavaSelenium-Git@users.noreply.github.com>
2023-08-10 12:39:57 +0530

    Added Files
    Modified readme.md and added java file

---
+++ FirstFileGitDesktop.java
@@ -0,0 +1,5 @@
+public class FirstFileGitDesktop{
+    public static void main(String[] args) {
+        System.out.println("First Java file created in Local Repo to be added");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DemoPuneGitDesktop
+First Line of the ReadMe file
+# Second Section
+New Java file added to the Repo
